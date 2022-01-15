3580596107faa0a8b9135733ae39a422164afdd9
samssi <samir.hamadi@gmail.com>
2022-01-16 01:53:42 +0200

    Triangle drawer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,9 +1,9 @@
-import { appState } from './state';
-import { Vector2d } from './vector';
+import { appState, createTriangle, Triangle } from './state';
 
 const rendererWidth = window.innerWidth;
 const rendererHeight = window.innerHeight;
-const scale = 1;
+const halfWidth = rendererWidth / 2;
+const halfHeight = rendererHeight / 2;
 
 const createCanvas = (element: HTMLElement): HTMLCanvasElement => {
   const canvas = document.createElement('canvas');
@@ -13,12 +13,32 @@ const createCanvas = (element: HTMLElement): HTMLCanvasElement => {
   return canvas;
 };
 
+/*
 const drawVector = (context: CanvasRenderingContext2D, vector2d: Vector2d): void => {
   context.beginPath();
   context.moveTo(rendererWidth / 2, rendererHeight / 2);
   context.lineTo(
     vector2d.x + (rendererWidth / 2) * scale,
     vector2d.y + (rendererHeight / 2) * scale,
+  );
+  context.stroke();
+}; */
+
+const drawTriangle = (context: CanvasRenderingContext2D, triangle: Triangle): void => {
+  context.beginPath();
+  context.moveTo(halfWidth, halfHeight);
+  context.lineTo(
+    triangle.aVector.x + halfWidth,
+    triangle.aVector.y + halfHeight,
+  );
+  context.lineTo(
+    triangle.bVector.x + triangle.aVector.x + halfWidth,
+    triangle.bVector.y + triangle.aVector.y + halfHeight,
+  );
+  context.moveTo(halfWidth, halfHeight);
+  context.lineTo(
+    triangle.cVector.x + halfWidth,
+    triangle.cVector.y + halfHeight,
   );
   context.stroke();
 };
@@ -31,11 +51,8 @@ export const update = (rootElement: HTMLElement): void => {
   }
 
   appState.update({
-    vector2ds: [
-      { x: 100, y: 0 },
-      { x: 0, y: 100 },
-    ],
+    triangles: [createTriangle({ x: 200, y: 0 }, { x: 0, y: 90 })],
   });
   console.log('tick');
-  appState.currentState().vector2ds.forEach((vector2d) => drawVector(context, vector2d));
+  appState.currentState().triangles.forEach((triangle) => drawTriangle(context, triangle));
 };
--- src/state.ts
+++ src/state.ts
@@ -1,12 +1,24 @@
 import { Vector2d } from './vector';
 
-interface State {
-  vector2ds: Vector2d[]
+export interface Triangle {
+  aVector: Vector2d
+  bVector: Vector2d,
+  cVector: Vector2d
 }
+
+interface State {
+  triangles: Triangle[]
+}
+
+export const createTriangle = (aVector: Vector2d, bVector: Vector2d): Triangle => ({
+  aVector: { x: aVector.x, y: -aVector.y },
+  bVector: { x: bVector.x, y: -bVector.y },
+  cVector: { x: aVector.x + bVector.x, y: aVector.y + -bVector.y },
+});
 
 export const appState = (() => {
   let stateContainer: State = {
-    vector2ds: [],
+    triangles: [],
   };
 
   return {
